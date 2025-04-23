8e50d7ef063f97f0b9a92b7e45e108f988a98b37
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-04-23 05:14:53 -0300

    Cleaning project, configuring tailwind and first component navbar

--- vue-cc/src/App.vue
+++ vue-cc/src/App.vue
@@ -1,39 +1,7 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'John Doe',
-      status: 'inactive',
-      tasks: ['Task one', 'Task two', 'Task three'],
-      link: 'https://google.com'
-    }
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending';
-      } else if (this.status === 'pending') {
-        this.status = 'inactive';
-      } else if (this.status === 'inactive') {
-        this.status = 'active';
-      }
-    }
-  }
-}
+<script setup>
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <h1>Vue Jobs</h1>
-  <p v-if="status === 'active'">{{ name }}</p>
-  <p v-else-if="status === 'pending'">pending</p>
-  <p v-else="status === 'inactive'">User is inactive</p>
-  <h3>Tasks</h3>
-  <ul>
-    <li v-for="task of tasks" :key="task">{{ task }}</li>
-  </ul>
-  <a v-bind:href="link">google.com</a>
-  <button v-on:click="toggleStatus">status</button>
-
+  <Navbar />
 </template>
-
-<style></style>
