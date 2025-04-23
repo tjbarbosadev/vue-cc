78a0b3680f08ed66a7e0efd0959f5e8be4dd66d9
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-04-23 04:32:34 -0300

    Understanding composition

---
+++ vue-cc/src/App2.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const name = ref('John Doe');
+const status = ref('active');
+const tasks = ref(['Task one', 'Task two', 'Task three']);
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending';
+  } else if (status.value === 'pending') {
+    status.value = 'inactive';
+  } else if (status.value === 'inactive') {
+    status.value = 'active';
+  }
+}
+
+</script>
+
+<template>
+  <h1>Vue Jobs</h1>
+  <p v-if="status === 'active'">{{ name }}</p>
+  <p v-else-if="status === 'pending'">pending</p>
+  <p v-else="status === 'inactive'">User is inactive</p>
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task of tasks" :key="task">{{ task }}</li>
+  </ul>
+  <button v-on:click="toggleStatus">status</button>
+
+</template>
+
+<style></style>
--- vue-cc/src/main.js
+++ vue-cc/src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App2.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
